4fb48bfa259717bb36317a4b3cf51cd5c155dd26
guillaume71 <guillaume271293@hotmail.fr>
2022-11-30 16:46:52 +0100

    bruteforce

--- bruteforce.py
+++ bruteforce.py
@@ -1,18 +1,28 @@
+import time
+import csv
+
+
+start_time = time.time()
+
+
 def bruteforce(depense_max, donnees, lst_actions_selectionees=[]):
     if donnees:
         """
-        les variables val1, lst_val1 représentent le résultat de la fonction (rentabilité max, liste action) sans l'action courante
+            les variables val1, lst_val1 représentent le résultat de la fonction
+            (rentabilité max, liste action) sans l'action courante
         """
+
         val1, lst_val1 = bruteforce(depense_max, donnees[1:], lst_actions_selectionees)
         action_selection = donnees[0]  # ici on selectionne une action dans la liste
         if action_selection[1] <= depense_max:
             """
-            on enleve le montant de l'action en cours de la dépense max et on ajoute cette action dans la liste des actions
+                on enleve le montant de l'action en cours de la dépense max
+                et on ajoute cette action dans la liste des actions
             """
             val2, lst_val2 = bruteforce(depense_max - action_selection[1], donnees[1:],
                                         lst_actions_selectionees + [action_selection])
             """
-            résultat de la meilleure rentabilité 
+                résultat de la meilleure rentabilité
             """
             if val1 < val2:
                 return val2, lst_val2
@@ -22,11 +32,12 @@ def bruteforce(depense_max, donnees, lst_actions_selectionees=[]):
         """
             On renvoie à la fin la meilleur rentabilité total et la liste des actions et le montant maximum trouvé
         """
-        return f"la rentabilité maximum obtenue est : \
-            {round(sum([i[1] * i[2] for i in lst_actions_selectionees]), 2)}", \
-            f"La depense maximum est : {sum([i[1] for i in lst_actions_selectionees])} euros, " \
-            f"avec ces actions: {[i[0] for i in lst_actions_selectionees]}"
-        # return sum([i[2] for i in lst_actions_selectionees]), lst_actions_selectionees
+        # return f"la rentabilité maximum obtenue est : \
+        #     {round(sum([i[1] * i[2] for i in lst_actions_selectionees]), 2)}", \
+        #     f"La depense maximum est : {sum([i[1] for i in lst_actions_selectionees])} euros, " \
+        #     f"avec ces actions: {[i[0] for i in lst_actions_selectionees]}"
+
+        return sum([i[2] for i in lst_actions_selectionees]), lst_actions_selectionees
 
 
 lst_actions = [
@@ -51,4 +62,6 @@ lst_actions = [
         ["action_19", 24, 0.21],
         ["action_20", 114, 0.18],
     ]
+
 print(bruteforce(500, lst_actions))
+print("\nTemps écoulé : ", time.time() - start_time, "seconds")
