5e091537ab60844d7374d4357b7546cb36303660
guillaume71 <guillaume271293@hotmail.fr>
2022-11-30 16:46:34 +0100

    optimized

--- optimized.py
+++ optimized.py
@@ -1,61 +1,57 @@
-def dynamic(depense_max, donnees):
-
-    """
-    Algorithme de programmation dynamique, sauvegardant le meilleur résultat à chaque itération,
-    et le comparant avec le meilleur résultat précédent.
-    """
+import time
 
 
-    # On crée ici une matrice où les colonnes correspondent aux dépenses et les lignes aux actions.
-    # Toutes les valeurs sont initialisés à 0
-    matrice = [[0 for x in range(depense_max + 1)] for x in range(len(donnees) + 1)]
+start_time = time.time()
 
-    # On fait ensuite une boucle sur les lignes actions
-    for line in range(1, len(donnees) + 1):
 
-        # On fait ici une boucle sur les colonnes dépenses
-        for c in range(1, depense_max + 1):
-            # On vérifie que le coût de l'action est inférieur à la colonne dépense où l'on se situe
-            if donnees[line-1][1] <= c:
+MAX_VALUE_INVEST = 500
 
-                # On rentre dans la matrice le maximum entre :
-                #  - la rentabilité max de la ligne précédente
-                #       -> matrice[l-1][c]
-                #  - la rentabilité de la dépense courante + (la solution optimisée de la dépense de la ligne d'avant)
-                #       -> donnees[l-1][2] + matrice[l-1][c-donnees[l-1][1]]
 
-                #  max (rentablilité courante + rentabilité ligne précedente arrivant au poids max,
-                # rentabilité max de la ligne précedente
-                matrice[line][c] = max(donnees[line-1][2] + matrice[line-1][c-donnees[line-1][1]], matrice[line-1][c])
-
-            # Si la dépense est supérieure à la dépense max, on récupére la solution de la ligne précédente
+def dynamic_search(list_actions, max_value=MAX_VALUE_INVEST):
+    matrix = [[0 for x in range(max_value + 1)] for x in range(len(list_actions) + 1)]
+    for i in range(1, len(list_actions) + 1):
+        for wallet in range(1, max_value + 1):
+            if list_actions[i - 1][1] <= wallet:
+                matrix[i][wallet] = max(list_actions[i - 1][2] + matrix[i - 1][int(wallet - list_actions[i - 1][1])],
+                                        matrix[i - 1][wallet])
             else:
-                matrice[line][c] = matrice[line-1][c]
-
-    # Rechercher les actions en fonction du résultats
-    w = depense_max
-    n = len(donnees)
+                matrix[i][wallet] = matrix[i - 1][wallet]
+    wallet = max_value
+    nb_actions = len(list_actions)
     actions_selection = []
+    while wallet >= 0 and nb_actions >= 0:
+        action = list_actions[nb_actions - 1]
+        if matrix[nb_actions][int(wallet)] == matrix[nb_actions - 1][int(wallet - action[1])] + action[2]:
+            actions_selection.append(action)
+            wallet -= action[1]
+        nb_actions -= 1
+    max_invest = MAX_VALUE_INVEST - wallet
+    return matrix[-1][-1], actions_selection, max_invest
 
-    # Tant que la dépense est >= 0 et qu'il reste des actions à parcourir
-    while w >= 0 and n >= 0:
 
-        # ici on récupére la dernière action, on parcours la liste à l'envers
-        a = donnees[n-1]
+lst_actions = [
+        ["action_01", 20, 0.05],
+        ["action_02", 30, 0.1],
+        ["action_03", 50, 0.15],
+        ["action_04", 70, 0.2],
+        ["action_05", 60, 0.17],
+        ["action_06", 80, 0.25],
+        ["action_07", 22, 0.07],
+        ["action_08", 26, 0.11],
+        ["action_09", 48, 0.13],
+        ["action_10", 34, 0.27],
+        ["action_11", 42, 0.17],
+        ["action_12", 110, 0.09],
+        ["action_13", 38, 0.23],
+        ["action_14", 14, 0.01],
+        ["action_15", 18, 0.03],
+        ["action_16", 8, 0.08],
+        ["action_17", 4, 0.12],
+        ["action_18", 10, 0.14],
+        ["action_19", 24, 0.21],
+        ["action_20", 114, 0.18],
+    ]
 
-        # Si la rentabilté en cours  == la rentabilité de la valeur en cours -
-        #                               la rentabilité de la valeur diminué de la dépense d'avant,
-        # alors on sait que cette action est à ajouté dans la liste
-        if matrice[n][w] == matrice[n-1][w - a[1]] + a[2]:
-            actions_selection.append(a)
+print(dynamic_search(lst_actions))
+print("\nTemps écoulé : ", time.time() - start_time, "seconds")
 
-            # on réduit ensuite la dépense max de la valeur de l'action selectionnée
-            w -= a[1]
-
-        # ensuite on passe à l'élément suivant
-        n -= 1
-
-    depense_total_selection = 0
-
-    for a in actions_selection:
-        depense_total_selection += a[1]
